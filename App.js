b71d4fcb8352cbe513ca66908a3009addc2ea2eb
Darran Kelinske <darran.kelinske@gmail.com>
2018-03-28 09:12:03 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import ToDoList from './src/components/TodoList'
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <ToDoList />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
